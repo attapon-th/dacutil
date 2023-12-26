4f48272b81b6c3a58c54aa4a547fd2e8ad25d949
Attapon Thanawong <attapon_srem@hotmail.com>
2023-12-26 14:12:28 +0700

    Modified: dacutil/__init__.py, dacutil/config.py, pyproject.toml

--- dacutil/__init__.py
+++ dacutil/__init__.py
@@ -1,10 +1,11 @@
 from dacutil.dateutil import datediff
 from dacutil.thai_mod11 import check_mod11
-from dacutil.config import get_config
+from dacutil.config import get_config, AdDict
 from dacutil.strutil import df_strip
 from dacutil.worker import worker
 
 __all__ = [
+    "AdDict",
     "datediff",
     "check_mod11",
     "get_config",
--- dacutil/config.py
+++ dacutil/config.py
@@ -1,22 +1,54 @@
-from addict import Dict as AdDict
-from urllib.parse import urlparse, ParseResult
 import os
+
+
+from addict import Dict as adDict
+from urllib.parse import urlparse, ParseResult
 from configparser import ConfigParser
-import requests
+from typing import Tuple, Optional, Dict
+from requests.auth import HTTPBasicAuth
+from requests import Response, get as req_get
 
 
-def get_config(uri_of_file: str):
+# Type alias for AdDict
+AdDict = adDict
+
+# error is config scheme not support
+ErrConfigSchemeNotSupport = Exception("Config scheme not support")
+
+
+def get_config(
+    uri_of_file: str,
+    basic_auth: Optional[Tuple[str, str]] = None,
+    headers: Optional[Dict[str, str]] = None,
+) -> AdDict:
+    """
+    Retrieves the configuration based on the provided URI of the file.
+
+    Args:
+        uri_of_file (str): The URI of the file containing the configuration.
+        basic_auth (Tuple[str, str] | None, optional): The basic authentication credentials. Defaults to None.
+        headers (dict[str, str] | None, optional): The headers. Defaults to None.
+
+    Returns:
+        AdDict: An `AdDict` object representing the retrieved configuration.
+    """
     conf = AdDict()
-    uri = uri_of_file
+    uri: str = uri_of_file
 
+    # Check if the URI is a local file
     if "://" not in uri:
-        conf: AdDict = get_config_file(uri)
+        # set the scheme to file
+        uri = "file://" + os.path.realpath(uri)
+
     # <scheme>://<netloc>/<path>;<params>?<query>#<fragment>
     u: ParseResult = urlparse(uri_of_file)
     if u.scheme == "http" or u.scheme == "https":
-        conf = get_config_url(uri)
-    if u.scheme == "file":
+        conf: AdDict = get_config_url(uri, basic_auth, headers)
+    elif u.scheme == "file":
         conf = get_config_file(uri.replace("file://", ""))
+    else:
+        raise ErrConfigSchemeNotSupport
+
     return conf
 
 
@@ -29,11 +61,22 @@ def get_config_file(filepath: str) -> AdDict:
     return AdDict()
 
 
-def get_config_url(url: str) -> AdDict:
+def get_config_url(
+    url: str,
+    basic_auth: Optional[Tuple[str, str]] = None,
+    headers: Optional[Dict[str, str]] = None,
+) -> AdDict:
     conf = AdDict()
-    r = requests.get(url)
+    bAuth: HTTPBasicAuth | None = HTTPBasicAuth(*basic_auth) if basic_auth else None
+    r: Response = req_get(url, timeout=30, auth=bAuth, headers=headers)
+
     if r.status_code >= 200 and r.status_code < 300:
-        c = ConfigParser()
-        c.read_string(r.text)
-        conf = AdDict(dict(c._sections))  # type: ignore
+        if url.endswith(".ini"):
+            c = ConfigParser()
+            c.read_string(r.text)
+            conf = AdDict(dict(c._sections))  # type: ignore
+        elif url.endswith(".json"):
+            conf = AdDict(r.json())
+        else:
+            raise ErrConfigSchemeNotSupport
     return conf
